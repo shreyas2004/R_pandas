8454a9d191cd8e1dfc69ddc6b9869a0f34bbbf26
shreyas2004 <shreyas1762004@gmail.com>
2024-12-09 23:42:37 +0530

    head,tail,info,describe,isnull,

--- panda1.py
+++ panda1.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
 data = pd.read_excel("D:\pandas\sample.xlsx")
-print(data)
-# Ensure pandas and an Excel library like openpyxl are installed.
+print(data.head(10)) #head() is used to quickly view the first few rows of a DataFrame for a quick preview.
+print(data.tail(10))#tail() is used to quickly view the last few rows of a DataFrame for a quick preview.
+#default value is 5
+print(data.info())#give inforation about data like datatype etc
+print(data.describe())#give describe the data
+print(data.isnull())#give null valuses as true
+print(data.isnull().sum())
